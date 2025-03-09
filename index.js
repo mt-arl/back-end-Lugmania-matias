41c3becc3116c0b1212c8b2ea7dcd786298a707f
mt-arl <matt_ariel@hotmail.com>
2025-03-08 21:36:41 -0500

    remove server port declaration and related logging

--- index.js
+++ index.js
@@ -16,8 +16,3 @@ app.use(express.json());
 // Rutas
 app.use('/api/users2', userRoutes2);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servitor corridor en el puerto ${PORT}`);
-});
-
